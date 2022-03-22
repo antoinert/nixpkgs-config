1296edc367ed71191a6ae9cc3b96ed5808630d6d
Anton Rautio <anton.rautio@gmail.com>
2022-03-22 15:20:17 +0200

    zsh

--- home.nix
+++ home.nix
@@ -34,5 +34,24 @@
         IdentityFile ~/.ssh/id_rsa
     '';
   };
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      # plugins = [ "git" "thefuck" ];
+      theme = "af-magic";
+    };
+  };
 }
 
